72fcb11f9e41f83fb53a6fa55a5b63d4fbba1fa6
Krisztian Cseh <csehkrisztian@outlook.com>
2025-10-25 11:12:53 +0200

    Code Added

--- CodeReview/Program.cs
+++ CodeReview/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool notUsed = true;
+            bool notModified = true;
+            if (notModified){string message = "alma körte banán"; Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}");
+                message = "piros zöld"; Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}");
+                message = "GPU CPU NPU FPU"; Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}\n{message.Split(" ")[3]}");}
+            else splitmethod("alma körte banán");
         }
+
+        static void splitmethod(string message)
+        {var parts = message.Split(" ");foreach (var part in parts) { Console.WriteLine(part); }}
+
+        static void unusedmethod() { Console.WriteLine("This method is never called."); }
     }
 }
