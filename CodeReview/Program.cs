28f492ca8d3f3030d5523d9fabe40f2e26504cb8
Krisztian Cseh <csehkrisztian@outlook.com>
2025-10-25 11:53:32 +0200

    Review fixes 2

--- CodeReview/Program.cs
+++ CodeReview/Program.cs
@@ -4,19 +4,19 @@
     {
         static void Main(string[] args)
         {
-            string message = "alma körte banán";
-            Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}");
-            message = "piros zöld";
-            Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}");
-            message = "GPU CPU NPU FPU";
-            Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}\n{message.Split(" ")[3]}");
+            SplitMethod("alma körte banán");
+            
+            SplitMethod("piros zöld");
+            
+            SplitMethod("GPU CPU NPU FPU");
         }
 
         static void SplitMethod(string message)
         {
-            var parts = message.Split(" ");
-            foreach (var part in parts)
-            { Console.WriteLine(part); }
+            foreach (var part in message.Split(" "))
+            {
+                Console.WriteLine(part);
+            }
         }
     }
 }
