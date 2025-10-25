096510a7d54ed5308c1b10bb4f441b4a5973b62f
Krisztian Cseh <csehkrisztian@outlook.com>
2025-10-25 11:38:08 +0200

    Review fixes

--- CodeReview/Program.cs
+++ CodeReview/Program.cs
@@ -4,17 +4,19 @@
     {
         static void Main(string[] args)
         {
-            bool notUsed = true;
-            bool notModified = true;
-            if (notModified){string message = "alma körte banán"; Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}");
-                message = "piros zöld"; Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}");
-                message = "GPU CPU NPU FPU"; Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}\n{message.Split(" ")[3]}");}
-            else splitmethod("alma körte banán");
+            string message = "alma körte banán";
+            Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}");
+            message = "piros zöld";
+            Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}");
+            message = "GPU CPU NPU FPU";
+            Console.WriteLine($"{message.Split(" ")[0]}\n{message.Split(" ")[1]}\n{message.Split(" ")[2]}\n{message.Split(" ")[3]}");
         }
 
-        static void splitmethod(string message)
-        {var parts = message.Split(" ");foreach (var part in parts) { Console.WriteLine(part); }}
-
-        static void unusedmethod() { Console.WriteLine("This method is never called."); }
+        static void SplitMethod(string message)
+        {
+            var parts = message.Split(" ");
+            foreach (var part in parts)
+            { Console.WriteLine(part); }
+        }
     }
 }
